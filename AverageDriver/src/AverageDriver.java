21e35a90234478c43edc91cde0179b611cec3b4d
AntonyoHutsona <antoyohutsona@gmail.com>
2015-08-15 23:49:23 -0500

    initial commit

---
+++ AverageDriver/src/AverageDriver.java
@@ -0,0 +1,18 @@
+/**
+ *@author ahutsona
+ * Student-ID:	36225268
+ * Class        CIS163AA
+ * Assignment:	Chapter_07_Assignment Averagr driver
+ */
+public class AverageDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Average average = new Average();
+		average.promptUser();
+    }
+}
+
